9bd348f5413eff1a207f67d58a8072e35dbe6c6a
Lepionnier92 <134221586+Lepionnier92@users.noreply.github.com>
2024-06-28 15:16:59 +0000

    #

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,8 @@
-// src/js/script.js
-
-/* Votre code JavaScript personnalisé */
-// Attend que le DOM soit entièrement chargé
-document.addEventListener('DOMContentLoaded', function () {
-    const apiUrl = 'https://api.paladium.games/v1/status';
-    const serverStatusElement = document.getElementById('serverStatus');
-
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            // Récupération des informations spécifiques du serveur
-            const javaGlobalStatus = data.java.global.status;
-            const javaGlobalPlayers = data.java.global.players;
-            const factions = data.java.factions;
-            const launcherStatus = data.launcher.status;
-
-            // Construction du contenu à afficher dans le <p>
-            let serverInfo = `Statut du serveur Java : ${javaGlobalStatus}, ${javaGlobalPlayers} joueurs en ligne<br>`;
-            serverInfo += `Statut du launcher : ${launcherStatus}<br><br>`;
-            serverInfo += 'Factions :<br>';
-            // Boucle à travers les factions
-            Object.keys(factions).forEach(faction => {
-                serverInfo += `- ${faction} : ${factions[faction]}<br>`;
-            });
-
-            // Affichage des données dans l'élément <p>
-            serverStatusElement.innerHTML = serverInfo;
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            serverStatusElement.textContent = 'Erreur lors de la récupération du statut du serveur';
-        });
-});
-
+document.getElementById('themeToggleBtn').addEventListener('click', function () {
+    const htmlElement = document.documentElement;
+    if (htmlElement.getAttribute('data-bs-theme') === 'dark') {
+      htmlElement.setAttribute('data-bs-theme', 'light');
+    } else {
+      htmlElement.setAttribute('data-bs-theme', 'dark');
+    }
+  });
